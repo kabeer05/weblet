87798b0bf0e82e241127f239e44cd53b71d54075
Kabeer Arora <kabeerarora13@gmail.com>
2024-09-03 19:04:10 +0530

    feat: UI changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,18 @@
-"use client";
-
 import CodeEditor from "@/components/CodeEditor";
-import { useState } from "react";
+import { loadLanguage } from "@uiw/codemirror-extensions-langs";
 
 export default function Home() {
-  const [code, setCode] = useState("console.log('Hello World')");
-
   return (
-    <main className="flex">
-      <section className="w-1/2 h-[100vh]">
+    <div className="h-screen w-full grid grid-cols-3 grid-rows-6 gap-4 px-6 py-6">
+      <div className="bg-dracula-bg col-span-2 row-span-6 rounded-lg">
         <CodeEditor
-          code={code}
-          onChange={() => {
-            setCode(code);
-          }}
+          code="console.log('Hello, World!')"
+          language={loadLanguage("javascript")!}
         />
-      </section>
-      <section className="w-1/2 h-[100vh]"></section>
-    </main>
+      </div>
+      <div className="bg-dracula-bg col-start-3 rounded-lg"></div>
+      <div className="bg-dracula-bg row-span-2 col-start-3 row-start-2 rounded-lg"></div>
+      <div className="bg-dracula-bg row-span-3 col-start-3 row-start-4 rounded-lg"></div>
+    </div>
   );
 }
--- components/CodeEditor.tsx
+++ components/CodeEditor.tsx
@@ -1,13 +1,35 @@
-import CodeMirror from "@uiw/react-codemirror";
+import CodeMirror, { Extension } from "@uiw/react-codemirror";
+import { draculaInit } from "@uiw/codemirror-theme-dracula";
+
+interface CodeEditorProps {
+  code: string;
+  language: Extension;
+  onChange?: (value: string) => void;
+}
 
 export default function CodeEditor({
   code,
   language,
   onChange,
-}: {
-  code: string;
-  onChange: (code: string) => void;
-  language?: any;
-}) {
-  return <CodeMirror value={code} height="200px" extensions={language} />;
+}: CodeEditorProps) {
+  return (
+    <CodeMirror
+      value={code}
+      onChange={onChange}
+      draggable={false}
+      basicSetup={{
+        lineNumbers: true,
+        bracketMatching: true,
+        closeBrackets: true,
+        autocompletion: true,
+        highlightActiveLine: false,
+        highlightSpecialChars: true,
+        syntaxHighlighting: true,
+        indentOnInput: true,
+      }}
+      theme={draculaInit()}
+      className="h-full w-full overflow-y-scroll rounded-lg"
+      extensions={[language]}
+    />
+  );
 }
